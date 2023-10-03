7a25e2647eaa2d406366cf4708e1d6910ae9c75e
apyo <amy_pyo@sfu.ca>
2023-10-03 02:06:59 -0700

    can parse socialgaming

--- tools/parser/parser_test.cpp
+++ tools/parser/parser_test.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <cassert>
 #include <cstdio>
 #include <memory>
@@ -7,36 +9,32 @@
 
 
 extern "C" {
-TSLanguage* tree_sitter_json();
+TSLanguage* tree_sitter_socialgaming();
 }
 
 
 int main() {
   // Create a language and parser.
-  ts::Language language = tree_sitter_json();
+  ts::Language language = tree_sitter_socialgaming();
   ts::Parser parser{language};
 
   // Parse the provided string into a syntax tree.
-  std::string sourcecode = "[1, null]";
+  std::string sourcecode = "configuration {"
+                            "name: \"\""
+                            "player range: (0, 0)"
+                            "audience: false"
+                            "setup: {}"
+                            "}";
   ts::Tree tree = parser.parseString(sourcecode);
 
   // Get the root node of the syntax tree.
   ts::Node root = tree.getRootNode();
 
   // Get some child nodes.
-  ts::Node array = root.getNamedChild(0);
-  ts::Node number = array.getNamedChild(0);
-
-  // Check that the nodes have the expected types.
-  assert(root.getType() == "document");
-  assert(array.getType() == "array");
-  assert(number.getType() == "number");
-
-  // Check that the nodes have the expected child counts.
-  assert(root.getNumChildren() == 1);
-  assert(array.getNumChildren() == 5);
-  assert(array.getNumNamedChildren() == 2);
-  assert(number.getNumChildren() == 0);
+  ts::Node configuration = root.getNamedChild(0);
+  ts::Node name = configuration.getChildByFieldName("name");
+  ts::Node playerRange = configuration.getChildByFieldName("player_range");
+  ts::Node hasAudience = configuration.getChildByFieldName("has_audience");
 
   // Print the syntax tree as an S-expression.
   auto treestring = root.getSExpr();
