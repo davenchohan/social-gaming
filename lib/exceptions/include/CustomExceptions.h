72d2d8b7141ca2b1a7234ed01613c36771fa55c9
Gabe Ko <gabek@sfu.ca>
2023-10-05 11:35:15 -0700

    Add implementation of all custom exceptions

--- lib/exceptions/include/CustomExceptions.h
+++ lib/exceptions/include/CustomExceptions.h
@@ -16,3 +16,27 @@ class IncompleteGameException : public std::exception{
     IncompleteGameException(char *name) : gameName(name) {}
     const std::string what();
 };
+
+class UnknownGameSpecException : public std::exception{
+    private:
+        char *gameSpec;
+    public:
+    UnknownGameSpecException(char *spec) : gameSpec(spec) {}
+    const std::string what();
+};
+
+class UnknownPlayerException : public std::exception{
+    private:
+        char *playerName;
+    public:
+    UnknownPlayerException(char *name) : playerName(name) {}
+    const std::string what();
+};
+
+class UnknownRequestException : public std::exception{
+    private:
+        char *requestStr;
+    public:
+    UnknownRequestException(char *request) : requestStr(request) {}
+    const std::string what();
+};
--- lib/exceptions/src/CustomExceptions.cpp
+++ lib/exceptions/src/CustomExceptions.cpp
@@ -8,3 +8,15 @@ const std::string UnknownGameException::what(){
 const std::string IncompleteGameException::what(){
     return "Error, Incomplete game spec filled for: " + std::string(this->gameName);
 }
+
+const std::string UnknownGameSpecException::what(){
+    return "Error, Incomplete game spec filled for: " + std::string(this->gameSpec);
+}
+
+const std::string UnknownPlayerException::what(){
+    return "Error, player not found: " + std::string(this->playerName);
+}
+
+const std::string UnknownRequestException::what(){
+    return "Error, invalid request: " + std::string(this->requestStr);
+}
