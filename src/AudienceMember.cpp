4d03fdfcf4963a35dd0ea3a8423d02b0856c1ac3
Daven Chohan <dca120@sfu.ca>
2023-10-07 12:26:43 -0700

    Created GameList and edited AudienceMember

--- src/AudienceMember.cpp
+++ src/AudienceMember.cpp
@@ -4,3 +4,13 @@
 AudienceMember::AudienceMember(const std::string& name, int userId) : User(name, userId) {
 
 }
+
+// Set audience member state
+void AudienceMember::SetAudienceState(const AudienceMemberState& state) {
+    audienceState = state;
+}
+
+// Get audience member state
+AudienceMember::AudienceMemberState AudienceMember::GetAudienceState() const {
+    return audienceState;
+}
--- src/include/AudienceMember.h
+++ src/include/AudienceMember.h
@@ -5,9 +5,23 @@
 
 class AudienceMember : public User {
 public:
+    // Enumeration to represent audience member states
+    enum AudienceMemberState {
+        Active,        //  They are spectating
+        Inactive,    // They have stopped spectating
+    };
+
     // Constructor
     AudienceMember(const std::string& name, int userId);
 
+    // Set the audiences member's state
+    void SetAudienceState(const AudienceMemberState& state);
+
+    // Get the audiences member's state
+    AudienceMemberState GetAudienceState() const;
+
+private:
+    AudienceMemberState audienceState;
 };
 
 #endif
