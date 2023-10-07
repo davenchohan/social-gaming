1440b5584e7e880d06769ea5790bc390b06c3cb1
Gabe Ko <gabek@sfu.ca>
2023-10-06 21:13:13 -0700

    Add implementation for custom exceptions

---
+++ lib/exceptions/src/CustomExceptions.cpp
@@ -0,0 +1,23 @@
+
+#include <string>
+#include "CustomExceptions.h"
+
+const std::string UnknownGameException::what(){
+    return "Error, Unknown Game: " + this->gameName;
+}
+
+const std::string IncompleteGameException::what(){
+    return "Error, Incomplete game spec filled for: " + this->gameName;
+}
+
+const std::string UnknownGameSpecException::what(){
+    return "Error, Unknown game spec: " + this->gameSpec;
+}
+
+const std::string UnknownPlayerException::what(){
+    return "Error, player not found: " + this->playerName;
+}
+
+const std::string UnknownRequestException::what(){
+    return "Error, invalid request: " + this->requestStr;
+}
