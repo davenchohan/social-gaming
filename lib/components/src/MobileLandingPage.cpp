8503fe614f9353cf4e5af76db09d05da8c822d7b
colinkirkby <78116947+colinkirkby@users.noreply.github.com>
2023-10-16 22:47:50 -0700

    added a clientwrapper to take dataobjects and request types

--- lib/components/src/MobileLandingPage.cpp
+++ lib/components/src/MobileLandingPage.cpp
@@ -10,12 +10,17 @@
 #include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
 #include "ftxui/dom/elements.hpp" 
 #include "JoinGamePage.h"
+#include "DataClasses.h"
+#include "ClientWrapper.h"
+#include "Constants.h"
 
 using namespace ftxui;
 
 namespace Pages{
 Component MobileLandingPage(int &pagenum, std::string &code, std::string &name, networking::Client &client){
     const int max_pagenum = 1; // starting 0
+    networking::ClientWrapper wrapper;
+    
     auto title1 = Renderer([] {
         return paragraph("Invite Code");
     });
@@ -44,9 +49,12 @@ Component MobileLandingPage(int &pagenum, std::string &code, std::string &name, 
                return filler();
             }),
             Button("Next", [&]{
+                wrapper.sendNoBody(constants::ReqType::DEMOGETGAMES, client);
             pagenum++; // todo: need to check if invite code entered/valid before proceeding
             }) | Maybe([&] {return pagenum < max_pagenum;}),
             Button("Join", [&]{
+                GetGame getGame = GetGame(code);
+                wrapper.sendReq(constants::ReqType::DEMOGETGAME, getGame,client);
                 // transfer page to game play page
             }) | Maybe([&] {return pagenum == max_pagenum;}),
         }) | flex,
