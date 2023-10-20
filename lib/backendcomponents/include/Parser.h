889789c466914188f012303433da4443b36fc8da
Gabe Ko <gabek@sfu.ca>
2023-10-20 15:13:24 -0700

    Add function signatures for parser library

--- lib/backendcomponents/include/Parser.h
+++ lib/backendcomponents/include/Parser.h
@@ -1,4 +1,11 @@
 #include <nlohmann/json.hpp>
-using json = nlohmann::json;
+using Json = nlohmann::json;
 #include <string>
 
+#include "RequestStruct.h"
+
+namespace ParserLib{
+    std::string ConstructRequest(RequestInfo);
+    std::string ConstructResponse(RequestInfo);
+    RequestInfo* parseJsonObjects(Json);
+}
