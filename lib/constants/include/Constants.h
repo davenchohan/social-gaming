8180480af6f5d2fb3e5ff6b326278e0441bc48bd
Risa Kawagoe <rkawagoe@sfu.ca>
2023-10-19 23:56:08 -0700

    created classes for managing collection of components and for dynamically component generation

--- lib/constants/include/Constants.h
+++ lib/constants/include/Constants.h
@@ -10,6 +10,9 @@ namespace constants
 enum class ReqType : char {
   GETGAMES, JOINGAME, CREATEGAME, VIEWGAME, UPDATEGAME, DEMOGETGAMES, DEMOGETGAME,CREATEGAMEFILLED, UPDATEPLAYER
 };
+enum class GameComponentType : char {
+  DISPLAY, SINGLE_SELECT, MULTI_SELECT, INPUT
+};
 
 // CONSIDER: having reqtype when handling response from server
 
@@ -25,6 +28,17 @@ to_string(const constants::ReqType request) noexcept {
     case constants::ReqType::UPDATEGAME:  return "ReqUpdateGame";
     case constants::ReqType::DEMOGETGAMES:  return "DemoReqGetGamesList";
     case constants::ReqType::DEMOGETGAME:  return "DemoReqGetGame";
+  }
+  __builtin_unreachable();
+}
+
+constexpr inline std::string_view
+to_string(const constants::GameComponentType component) noexcept {
+  switch (component) {
+    case constants::GameComponentType::DISPLAY:   return "Display";
+    case constants::GameComponentType::SINGLE_SELECT:   return "SingleSelect";
+    case constants::GameComponentType::MULTI_SELECT: return "MultiSelect";
+    case constants::GameComponentType::INPUT: return "Input";
   }
   __builtin_unreachable();
 }
