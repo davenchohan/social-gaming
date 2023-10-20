f5d01e58a15ada2c8d5e7a0720f148c3f3312902
Gabe Ko <gabek@sfu.ca>
2023-10-20 15:13:24 -0700

    Add function signatures for parser library

---
+++ lib/backendcomponents/src/Parser.cpp
@@ -0,0 +1,13 @@
+#include "Parser.h"
+
+std::string ConstructRequest(RequestInfo someReq){
+    return "empty";
+}
+
+std::string ConstructResponse(RequestInfo someReq){
+    return "empty";
+}
+
+RequestInfo* parseJsonObjects(Json someJson){
+    return nullptr;
+}
