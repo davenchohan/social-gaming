6059a0707895303a26c263a2c26802c4f15b876a
Gabe Ko <gabek@sfu.ca>
2023-10-23 16:01:33 -0700

    Add executable for parser library: plRunner

---
+++ tools/server/plRunner.cpp
@@ -0,0 +1,23 @@
+#include "CustomExceptions.h"
+#include "Player.h"
+#include "Game.h"
+#include "GameSessionHandler.h"
+#include "GameVariable.h"
+#include "AudienceMember.h"
+#include "GameList.h"
+#include "Parser.h"
+
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+int main(){
+    return 0;
+}
