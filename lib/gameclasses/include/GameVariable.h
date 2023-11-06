3a852f64f9b2ce7e4b6d328cede9b91953dc9832
dca120 <dca120@sfu.ca>
2023-11-06 21:02:49 +0000

    Adding operator overloads for custom classes

--- lib/gameclasses/include/GameVariable.h
+++ lib/gameclasses/include/GameVariable.h
@@ -2,18 +2,46 @@
 #define GAMEVARIABLE_H
 
 #include <string>
+#include <variant>
+#include <vector>
+#include <map>
+#include <stdexcept>
 
 class GameVariable {
 public:
-    // Constructor
-    GameVariable(const std::string& name, const std::string& value);
+    // Constructors
+    GameVariable(const int& id);
+    GameVariable(const std::string& name, const int id, const int& intValue);
+    GameVariable(const std::string& name, const int id, const double& doubleValue);
+    GameVariable(const std::string& name, const int id, const std::string& stringValue);
 
-    // Get the value of the variable
-    std::string GetVariableValue() const;
+    // Type retrieval functions
+    bool IsInt() const;
+    bool IsDouble() const;
+    bool IsString() const;
+
+    // Templated getter for data
+    // example use: variable.GetData<int>();
+    template <typename T>
+    T GetData() const {
+        try {
+            return std::get<T>(data);
+        } catch (const std::bad_variant_access& e) {
+            throw std::runtime_error("Requested type does not match the variant type.");
+        }
+    }
+
+    std::string GetName() const;
+
+    bool operator==(const GameVariable& other) const;
+    bool operator<(const GameVariable& other) const;
+
+    int GetVariableId() const;
 
 private:
+    std::variant<int, double, std::string> data;
+    int id;
     std::string variableName;
-    std::string variableValue;
 };
 
 #endif
