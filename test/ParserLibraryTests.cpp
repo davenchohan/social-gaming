ff86dad4e53afad5075604a8f3eb733ee66d4567
gabek <gabek@sfu.ca>
2023-11-07 06:00:31 +0000

    Add 2 parser library tests

--- test/ParserLibraryTests.cpp
+++ test/ParserLibraryTests.cpp
@@ -118,4 +118,28 @@ TEST(ParserLibraryTests, TestRequestParserConstructor){
     EXPECT_EQ(generated, expected);
 }
 
+TEST(ParserLibraryTests, TestRequestParserAddPlayers){
+    std::vector<Player> players = {Player{"Gabe", 1234}, Player{"Peter", 1212}};
+    std::for_each(players.begin(), players.end(), [](auto &item){
+        item.SetPlayerState(Player::PlayerState::Active);
+    });
+    RequestConstructor con;
+    con.appendItem("Players", players);
+    auto generated = con.ConstructRequest();
+    auto expected = "{\"Players\":[{\"id\":1234,\"name\":\"Gabe\",\"playerState\":\"Active\"},{\"id\":1212,\"name\":\"Peter\",\"playerState\":\"Active\"}]}";
+    EXPECT_EQ(generated, expected);
+}
+
+TEST(ParserLibraryTests, TestRequestParserAddMembers){
+    std::vector<AudienceMember> members = { AudienceMember{"Gabe", 1234}, AudienceMember{"Peter",1111}};
+    std::for_each(members.begin(), members.end(), [](auto &item){
+        item.SetAudienceState(AudienceMember::AudienceMemberState::Active);
+    });
+    RequestConstructor con;
+    con.appendItem("AudienceMembers", members);
+    auto generated = con.ConstructRequest();
+    auto expected = "{\"AudienceMembers\":[{\"audienceMemberState\":\"Active\",\"id\":1234,\"name\":\"Gabe\"},{\"audienceMemberState\":\"Active\",\"id\":1111,\"name\":\"Peter\"}]}";
+    EXPECT_EQ(generated, expected);
+}
+
 
